dbebe4bc4da70bdbf629639acc4f758f4d081af8
Angel Lopez Pol <angellopezpol1254@gmail.com>
2024-02-20 22:14:37 -0500

    more function prototypes

--- include/RudaDI/rudadi.h
+++ include/RudaDI/rudadi.h
@@ -48,31 +48,109 @@ public:
 
     /// @brief Destroys window and associated resources
     /// @param window RudaDIWindow object to destroy
-    void rudaDestroyWindow(RudaDIWindow window);
+    void rudaDestroyWindow(RudaDIWindow* window);
 
     /// @brief Tracks state of whether or not window should close, useful for polling on while loops
     /// @param window window we are checking
     /// @return boolean true or false
-    bool rudaWindowShouldClose(RudaDIWindow window);
+    bool rudaWindowShouldClose(RudaDIWindow* window);
 
     /// @brief Set whether window should close for next poll
     /// @param window window we are setting value for
     /// @param value True for yes, False for no
-    void rudaSetWindowShouldClose(RudaDIWindow window, bool value);
+    void rudaSetWindowShouldClose(RudaDIWindow* window, bool value);
 
     /// @brief returns title of window
     /// @param window window we are getting title of
     /// @returns title string
-    string rudaGetWindowTitle(RudaDIWindow window);
+    string rudaGetWindowTitle(RudaDIWindow* window);
 
     /// @brief returns title of window
     /// @param window window we are setting title of
     /// @param title string title we set window to
-    void rudaSetWindowTitle(RudaDIWindow window, string title);
+    void rudaSetWindowTitle(RudaDIWindow* window, string title);
 
-    void rudaSetWindowIcon(RudaDIWindow window,  string imagePath);
-    int* rudaGetWindowPos(RudaDIWindow window);
-    void rudaSetWindowPos(RudaDIWindow window, int x, int y);
-    int* rudaGetWindowSize(RudaDIWindow window);
-    void rudaSetWindowSize(RudaDIWindow window, int x, int y);
+    void rudaSetWindowIcon(RudaDIWindow* window,  string imagePath);
+    int* rudaGetWindowPos(RudaDIWindow* window);
+    void rudaSetWindowPos(RudaDIWindow* window, int x, int y);
+    int* rudaGetWindowSize(RudaDIWindow* window);
+    void rudaSetWindowSize(RudaDIWindow* window, int x, int y);
+    // https://github.com/glfw/glfw/blob/8f2f766f0d2ed476c03a2ae02e48ac41a9602b03/include/GLFW/glfw3.h#L3700
+    
+    /// @brief Retrieves the size of the frame of the window, including in-screen coords of each edge of the frame
+    /// @param window window to retrieve size for
+    /// @return array of int values, corresponding to left, top, right, and bottom edge.
+    int* rudaGetWindowFrameSize(RudaDIWindow* window);
+    // Minify window
+    void rudaIconifyWindow(RudaDIWindow* window);
+    // Maximize window
+    void rudaMaximizeWindow(RudaDIWindow* window);
+    // Restore window to non-maxified non-minified state
+    void rudaRestoreWindow(RudaDIWindow* window);
+
+    void rudaShowWindow(RudaDIWindow* window);
+    
+    void rudaHideWindow(RudaDIWindow* window);
+
+    void rudaFocusWindow(RudaDIWindow* window);
+
+    void rudaRequestWindowAttention(RudaDIWindow* window);
+
+    /// @brief Returns the monitor that the window uses for full screen mode.
+    /// @param window window whose monitor handle we are trying to get
+    /// @return monitor handle that corresponds to fullscreened window
+    RudaDIMonitor rudaGetWindowMonitor(RudaDIWindow window);
+
+    /// @brief Sets the mode, monitor, video mode, and placement of a window that uses fullscreen mode
+    /// @param window RudaDI window object
+    /// @param monitor RudaDI monitor object
+    /// @param xpos x position of window
+    /// @param ypos y position of window
+    /// @param width width of window (screen coords)
+    /// @param height height of window (screen cords_)
+    /// @param refreshRate desired refresh rate of mode (optional)
+    void rudaSetWindowMonitor(RudaDIWindow* window, GLFWmonitor* monitor, int xpos, int ypos, int width, int height, int refreshRate);
+
+    /// @brief returns the value of an attribute of the specified window or its Ruda context.
+    /// @param window window object
+    /// @param attribute int/enum representation of Ruda context attribute
+    /// @return attribute value ( as an int/enum)
+    int rudaGetWindowAttrib(RudaDIWindow* window, int attribute);
+    void rudaSetWindowAttrib(RudaDIWindow* window, int attribute, int value);
+    /// @brief Sets function as callback on window resizes
+    /// @param window window which we are setting callback for
+    /// @param callbackFunc the callback func, which is given the width and height of content window post-resize
+    void rudaSetWindowSizeCallback(RudaDIWindow* window, void (*callbackFunc)(int, int));
+    void rudaSetWindowCloseCallback(RudaDIWindow* window, void (*callbackFunc)());
+    void rudaSetWindowFocusCallback(RudaDIWindow* window, void(*callbackFunc)());
+    void rudaSetWindowIconifyCallback(RudaDIWindow* window, void(*callbackFunc)(RudaDIWindow, int));
+    void rudaSetWindowMaximizeCallback(RudaDIWindow* window, void(*callbackFunc)(RudaDIWindow, int));
+    // Pretty sure this is relevant for resizing textures/images attached to a framebuffer since it doesn't get done automatically
+    void rudaSetFramebufferSizeCallback(RudaDIWindow* window, void(*callbackFunc)(RudaDIWindow, float, float));
+    void rudaSetWindowContenntScaleCallback(RudaDIWindow* window, void(*callbackFunc)(RudaDIWindow, float, float));
+
+    /// @brief processes all pending events in queue (event listening on another thread, implemented by x11, see _glfwPollEventsX11)
+    void rudaPollEvents();
+    /// @brief Waits for events (with timeout of 1000 by default) and blocks until an event is encountered (then handles them)
+    void rudaWaitForEvents();
+    /// @brief Waits for events (with customizable timeout and blocks until an event is encountered (then handles them)
+    void rudaWaitForEvents(double timeoutMs);
+    
+    //void rudaPostEmptyEvent();
+    //int rudaGetInputMode(RudaDIWindow* window, int mode);
+    //void rudaSetInputMode(RudaDIWindow* window, int mode, int value)
+    //int rudaRawMouseMotionSupported();
+    //string rudaGetKeyName(int key, int scancode);
+    //int rudaGetKeyScancode(int key);
+    //int rudaGetKey(RudaDIWindow* window, int key);
+    //int rudaGgtMouseButton(RudaDIWindow* window, int button);
+
+    /// @brief Retrieves mouse position on window content area (starting from top left)
+    /// @param window RudaDIWindow object
+    /// @return array of double values, corresponding to xpos and ypos of where the cursor is relative to left edge of content area
+    double* rudaGetMouseButton(RudaDIWindow* window);
+
+    RudaDICursor* rudaCreateStandardCursor()
+
+
 }
